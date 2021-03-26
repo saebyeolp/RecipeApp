ece173310e89320a789af02874a378013f74de53
saebyeolp <saebyeolpak@gmail.com>
2021-03-25 21:38:01 -0700

    Updates

---
+++ precache-manifest.da502da71b87cf43fae9d0393e03849c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2a29c00ff6ecb39230cabf400ede8a6",
+    "url": "/recipeapp/index.html"
+  },
+  {
+    "revision": "3f6f79923ce1baad7bf4",
+    "url": "/recipeapp/static/css/main.32ec329a.chunk.css"
+  },
+  {
+    "revision": "08cd2dbd7279a010e76d",
+    "url": "/recipeapp/static/js/2.74bd1aee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipeapp/static/js/2.74bd1aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6f79923ce1baad7bf4",
+    "url": "/recipeapp/static/js/main.24165fa7.chunk.js"
+  },
+  {
+    "revision": "26cd52a6ff406fac7c6e",
+    "url": "/recipeapp/static/js/runtime-main.14adba4c.js"
+  }
+]);
